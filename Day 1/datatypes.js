64be0e87c026054a4bddba0a225c899e10b4c6d7
Jimish Gajjar <jimish.gajjar@gmail.com>
2022-10-11 22:04:21 +0530

    Day 1 Exercise

---
+++ Day 1/datatypes.js
@@ -0,0 +1,23 @@
+let v1;
+let v2;
+let v3;
+let v4;
+
+let v_1 = 1;
+let v_2 = 1.5;
+let v_3 = "Str";
+let v_4 = true;
+
+let firstName1;
+let lastName1;
+let maritalStatus1;
+let country1;
+let age1;
+
+let firstName, lastName, maritalStatus, country, age;
+
+let myAge = 25;
+let yourAge = 30;
+
+console.log("I am " + myAge + " years old.");
+console.log("You are " + yourAge + " years old.");
---
+++ Day 1/variable.js
@@ -0,0 +1,10 @@
+str = "Hello";
+bol = true;
+un = undefined;
+nu = null;
+
+
+console.log(typeof str);
+console.log(typeof bol);
+console.log(typeof un);
+console.log(typeof nu);
